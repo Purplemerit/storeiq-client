34b2437ecc27202b57292355bab83596f87bc8d4
Avinash Poojary <avinashpoojary651@gmail.com>
2025-11-05 10:50:38 +0530

    tools gap fix

--- src/pages/Tools.tsx
+++ src/pages/Tools.tsx
@@ -230,7 +230,7 @@ const Tools = () => {
       </section>
 
       {/* Tools Grid */}
-      <section className="py-20 px-8">
+      <section className="pt-0 pb-20 px-8">
         <div className="max-w-7xl mx-auto">
           <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
             {tools.map((tool, index) => {
